761720552e570a31cecf23f2c24cf9dbe2a86f33
David <yoxisem544@gmail.com>
2015-09-10 10:49:24 +0800

    fix badge bug

--- ColorgyCourse/AppDelegate.swift
+++ ColorgyCourse/AppDelegate.swift
@@ -115,11 +115,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-        UIApplication.sharedApplication().applicationIconBadgeNumber = 0
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        UIApplication.sharedApplication().applicationIconBadgeNumber = 0
         // Flurry
         if Release().mode {
             Flurry.logEvent("v3.0: User Start Application, applicationDidBecomeActive")
