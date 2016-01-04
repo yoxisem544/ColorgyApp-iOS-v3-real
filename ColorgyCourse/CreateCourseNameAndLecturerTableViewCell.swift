f6cc314f243a652070b6fddedbea065e3b6138d7
David <yoxisem544@gmail.com>
2016-01-04 16:46:46 +0800

    add CreateCourseNameAndLecturerTableViewCell.swift

---
+++ ColorgyCourse/CreateCourseNameAndLecturerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CreateCourseNameAndLecturerTableViewCell.swift
+//  ColorgyCourse
+//
+//  Created by David on 2016/1/4.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+import UIKit
+
+class CreateCourseNameAndLecturerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
