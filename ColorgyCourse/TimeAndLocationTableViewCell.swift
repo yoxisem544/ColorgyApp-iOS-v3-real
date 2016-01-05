32d9c40b893336392ea19ac3470a75f39a7f2b34
David <yoxisem544@gmail.com>
2016-01-05 11:48:34 +0800

    configure tiem and location cell swift

--- ColorgyCourse/TimeAndLocationTableViewCell.swift
+++ ColorgyCourse/TimeAndLocationTableViewCell.swift
@@ -8,12 +8,40 @@
 
 import UIKit
 
+protocol TimeAndLocationTableViewCellDelegate {
+    func didTapOnTimeView()
+    func didTapOnLocationView()
+}
+
 class TimeAndLocationTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var timeBackgroundView: UIView!
+    @IBOutlet weak var locationBackgroundView: UIView!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var locationTextField: UITextField!
+    
+    var time: String?
+    var location: String?
+    var cellIndex: Int?
+    
+    var delegate: TimeAndLocationTableViewCellDelegate?
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         self.selectionStyle = .None
+        timeBackgroundView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "tapOnTimeView"))
+        locationBackgroundView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "tapOnLocationView"))
+    }
+    
+    func tapOnTimeView() {
+        print("on time")
+        delegate?.didTapOnTimeView()
+    }
+    
+    func tapOnLocationView() {
+        print("on location")
+        delegate?.didTapOnLocationView()
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
