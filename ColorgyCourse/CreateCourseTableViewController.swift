03114d04154f4281389453fae9ec8f31fdae9c48
David <yoxisem544@gmail.com>
2016-01-06 14:59:15 +0800

    able to show 3 section on tableview

--- ColorgyCourse/CreateCourseTableViewController.swift
+++ ColorgyCourse/CreateCourseTableViewController.swift
@@ -12,21 +12,17 @@ class CreateCourseTableViewController: UIViewController, UITableViewDataSource, 
     
     @IBOutlet weak var createCourseTableView: UITableView!
     
-    var dummyData: [TimeAndLocationTableViewCell]?
     var dummyDataLocation: [String?]?
     var dummyDataTime: [String?]?
+    let timeAndLocationSection: Int = 1
     @IBAction func createDumpData() {
-        if dummyData != nil {
-//            let cell = createCourseTableView.dequeueReusableCellWithIdentifier(Storyboard.timeAndLocationIdentifier) as! TimeAndLocationTableViewCell
-            let c = TimeAndLocationTableViewCell()
-            dummyData!.append(c)
+        if dummyDataLocation != nil {
             dummyDataLocation?.append("")
             dummyDataTime?.append("")
-            createCourseTableView.reloadSections(NSIndexSet(index: 0), withRowAnimation: .Fade)
-            let indexPath = NSIndexPath(forRow: dummyData!.count - 1, inSection: 0)
+            createCourseTableView.reloadSections(NSIndexSet(index: timeAndLocationSection), withRowAnimation: .Fade)
+            let indexPath = NSIndexPath(forRow: dummyDataLocation!.count - 1, inSection: timeAndLocationSection)
             createCourseTableView.scrollToRowAtIndexPath(indexPath, atScrollPosition: .Middle, animated: true)
         }
-        
     }
 
     override func viewDidLoad() {
@@ -45,7 +41,6 @@ class CreateCourseTableViewController: UIViewController, UITableViewDataSource, 
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-        dummyData = []
         dummyDataLocation = []
         dummyDataTime = []
     }
@@ -63,33 +58,64 @@ class CreateCourseTableViewController: UIViewController, UITableViewDataSource, 
 
     // MARK: - Table view data source
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 3
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-        if dummyData != nil {
-            return dummyData!.count
-        } else {
-            return 10
+        switch section {
+        case 0: // name and lecturer
+            return 1
+        case 1: // time and location
+            if dummyDataLocation != nil {
+                return dummyDataLocation!.count
+            } else {
+                return 10
+            }
+        case 2: // footer
+            return 1
+        default:
+            return 0
         }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCellWithIdentifier(Storyboard.timeAndLocationIdentifier, forIndexPath: indexPath) as! CreateCourseNameAndLecturerTableViewCell
-//        let c = tableView.dequeueReusableCellWithIdentifier(Storyboard.timeAndLocationIdentifier, forIndexPath: indexPath) as! TimeAndLocationTableViewCell
-        // Configure the cell...
-//        c.delegate = self
-//        c.cellIndex = indexPath.row
-//        c.timeLabel?.text = dummyDataTime![indexPath.row]
-//        c.locationTextField?.text = dummyDataLocation![indexPath.row]
-//        print(c.cellIndex)
-        
-        let a = tableView.dequeueReusableCellWithIdentifier(Storyboard.newTimeAndLocationIdentifier, forIndexPath: indexPath) as! ContinueAddTimeAndLocationTableViewCell
-        return a
 
-//        return c
+        switch indexPath.section {
+        case 0: // name and lecturer
+            let c = tableView.dequeueReusableCellWithIdentifier(Storyboard.nameAndLecturerIdentifier, forIndexPath: indexPath) as! CreateCourseNameAndLecturerTableViewCell
+            
+            return c
+        case 1: // time and location
+            let c = tableView.dequeueReusableCellWithIdentifier(Storyboard.timeAndLocationIdentifier, forIndexPath: indexPath) as! TimeAndLocationTableViewCell
+            
+            c.delegate = self
+            c.cellIndex = indexPath.row
+            c.timeLabel?.text = dummyDataTime![indexPath.row]
+            c.locationTextField?.text = dummyDataLocation![indexPath.row]
+            
+            return c
+        case 2: // footer
+            let c = tableView.dequeueReusableCellWithIdentifier(Storyboard.newTimeAndLocationIdentifier, forIndexPath: indexPath) as! ContinueAddTimeAndLocationTableViewCell
+            
+            return c
+        default:
+            return tableView.dequeueReusableCellWithIdentifier(Storyboard.newTimeAndLocationIdentifier, forIndexPath: indexPath) as! ContinueAddTimeAndLocationTableViewCell
+        }
+        
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 122.0
+        switch indexPath.section {
+        case 0:
+            return 88.0
+        case 1:
+            return 122.0
+        case 2:
+            return 122.0
+        default:
+            return 44.0
+        }
     }
 
 }
@@ -109,9 +135,8 @@ extension CreateCourseTableViewController : TimeAndLocationTableViewCellDelegate
     }
     
     func didPressDeleteButtonAtIndex(index: Int) {
-        dummyData?.removeAtIndex(index)
         dummyDataLocation?.removeAtIndex(index)
         dummyDataTime?.removeAtIndex(index)
-        createCourseTableView.reloadSections(NSIndexSet(index: 0), withRowAnimation: .Fade)
+        createCourseTableView.reloadSections(NSIndexSet(index: timeAndLocationSection), withRowAnimation: .Fade)
     }
 }
