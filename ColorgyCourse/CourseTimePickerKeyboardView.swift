8683b73cc84d3f2a4d8f057048a65dbbbf4981f9
David <yoxisem544@gmail.com>
2016-01-07 15:08:12 +0800

    configuring pickerview

--- ColorgyCourse/CourseTimePickerKeyboardView.swift
+++ ColorgyCourse/CourseTimePickerKeyboardView.swift
@@ -9,18 +9,40 @@
 import UIKit
 
 class CourseTimePickerKeyboardView: UIView {
+    
+    private var pickerView: UIPickerView?
+    private var content: [[Int]]?
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.frame.size.height = 216.0
         self.backgroundColor = UIColor.greenColor()
-        let p = UIPickerView()
-        print(p.frame)
-        self.addSubview(p)
+        pickerView = UIPickerView()
+        pickerView?.delegate = self
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+}
 
+extension CourseTimePickerKeyboardView : UIPickerViewDataSource {
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if content != nil {
+            return content![component].count
+        } else {
+            return 0
+        }
+    }
+    
+}
+
+extension CourseTimePickerKeyboardView : UIPickerViewDelegate {
+    
 }
