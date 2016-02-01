faea760e0e366ad8b5691d244301598c3eb6b288
David <yoxisem544@gmail.com>
2016-02-01 17:13:01 +0800

    clean up

--- ColorgyCourse/AppDelegate.swift
+++ ColorgyCourse/AppDelegate.swift
@@ -19,9 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-		
-//		HintViewSettings.resetSettings()
-//		HintViewSettings.setAppFirstLaunchNavigationViewShown()
+
         // reset all jobs
         ColorgyAPITrafficControlCenter.unQueueAllJobs()
         
