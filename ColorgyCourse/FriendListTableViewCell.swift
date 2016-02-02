d42a271c02d270cb83307dbb9730bb8d1de61571
David <yoxisem544@gmail.com>
2016-02-02 11:39:09 +0800

    create friend list

---
+++ ColorgyCourse/FriendListTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  FriendListTableViewCell.swift
+//  ColorgyCourse
+//
+//  Created by David on 2016/2/2.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+import UIKit
+
+class FriendListTableViewCell: UITableViewCell {
+
+	@IBOutlet weak var userProfileImageView: UIImageView!
+	@IBOutlet weak var userNameLabel: UILabel!
+	@IBOutlet weak var userQuestionLabel: UILabel!
+	@IBOutlet weak var userLastMessageLabel: UILabel!
+	@IBOutlet weak var timeStampLabel: UILabel!
+	
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+		userProfileImageView.clipsToBounds = true
+		userProfileImageView.layer.cornerRadius = userProfileImageView.bounds.width / 2
+		
+		self.selectionStyle = .None
+		self.backgroundColor = UIColor.clearColor()
+		self.contentView.backgroundColor = UIColor.clearColor()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ColorgyCourse/FriendListViewController.swift
@@ -0,0 +1,43 @@
+//
+//  FriendListViewController.swift
+//  ColorgyCourse
+//
+//  Created by David on 2016/2/2.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+import UIKit
+
+class FriendListViewController: UIViewController {
+
+	@IBOutlet weak var friendListTableView: UITableView!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+		friendListTableView.delegate = self
+		friendListTableView.dataSource = self
+		friendListTableView.estimatedRowHeight = 120
+		friendListTableView.rowHeight = UITableViewAutomaticDimension
+		friendListTableView.separatorInset.left = 50 + 16 + 8
+		friendListTableView.backgroundColor = UIColor.clearColor()
+    }
+	
+	struct Storyboard {
+		static let FriendListCellIdentifier = "Friend List Cell"
+	}
+}
+
+
+extension FriendListViewController : UITableViewDataSource, UITableViewDelegate {
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 10
+	}
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier(Storyboard.FriendListCellIdentifier, forIndexPath: indexPath) as! FriendListTableViewCell
+		return cell
+	}
+}
