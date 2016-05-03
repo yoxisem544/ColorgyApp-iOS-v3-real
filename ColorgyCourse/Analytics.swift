a03c5127e81231d20352ccd99c820ee39e32efb7
David Lin <yoxisem544@gmail.com>
2016-05-03 10:37:14 +0800

    add analytics class

---
+++ ColorgyCourse/Analytics.swift
@@ -0,0 +1,13 @@
+//
+//  Analytics.swift
+//  ColorgyCourse
+//
+//  Created by David on 2016/5/3.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+import Foundation
+
+class Analytics {
+	
+}
